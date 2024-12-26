b846d15f6e3583457fa5276ccec731792f17bed5
swanandchandan <swanandchandan130@gmail.com>
2024-12-26 17:54:32 +0530

    latest

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dummy_tradebattle/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Dummy_TradeBattle());
+    await tester.pumpWidget(const DummyTradeBattle());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
